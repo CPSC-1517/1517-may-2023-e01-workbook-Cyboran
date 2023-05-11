ab8e61a5acd2cdd4bf15b934e72c65b62899c0ff
Cyboran <cyboran@live.ca>
2023-05-10 18:40:09 -0600

    added a do while loop to allow user to run the loop for as long as they like

--- RionMurphyGitHubPracticeMay10/RionMurphyGitHubPracticeMay10/Program.cs
+++ RionMurphyGitHubPracticeMay10/RionMurphyGitHubPracticeMay10/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Greetings Creator. It is a pleasure to meet you in this world of github.");
 Console.WriteLine("We will be making use of examples to show how github commits work.");
+
+int userEntry = 0, i = 0;
+
+do
+{
+    i++;
+    Console.WriteLine($"Iteration #{i}");
+    Console.Write("\nPlease enter 1 to continue and 0 to exit");
+    userEntry = int.Parse( Console.ReadLine() );
+} while (userEntry != 0)
