a595733b89348a713ca347a660984cf4d34c3dab
Cyboran <cyboran@live.ca>
2023-05-10 18:27:08 -0600

    Added additional lines to file
    changed original message and added a new line

--- RionMurphyGitHubPracticeMay10/RionMurphyGitHubPracticeMay10/Program.cs
+++ RionMurphyGitHubPracticeMay10/RionMurphyGitHubPracticeMay10/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Greetings Creator. It is a pleasure to meet you in this world.");
+Console.WriteLine("Greetings Creator. It is a pleasure to meet you in this world of github.");
+Console.WriteLine("We will be making use of examples to show how github commits work.");
