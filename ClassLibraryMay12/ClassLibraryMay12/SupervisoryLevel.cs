4ce060b7161cb29ab9e5d4fb247cb453b481f09b
Cyboran <cyboran@live.ca>
2023-05-12 18:45:06 -0600

    Created a program to demonstrate knowledge on programming from CPSC1012

---
+++ ClassLibraryMay12/ClassLibraryMay12/SupervisoryLevel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryMay12
+{
+    public enum SupervisoryLevel
+    {
+        // summary
+        // enum names are strings representing an integer value
+        // by default the integer values start at 0 and increment by 1
+        // one could assign their own values to each of the enum names
+        Entry,
+        TeamMember,
+        TeamLeader,
+        Supervisor,
+        DepartmentHead,
+        Owner
+    }
+}
