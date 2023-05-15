0da1f5575e3bd1e276a8aa575dabd83d79741f71
Cyboran <cyboran@live.ca>
2023-05-15 17:10:46 -0600

    created a file for May 15 for easier tracking for myself

---
+++ ClassLibraryMay15/ClassLibraryMay15/Employment.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassLibraryMay15
+{
+    public class Employment
+    {
+        private SupervisoryLevel _Level;
+        private string _Title;
+        private string _Years;
+
+        // summary
+        // Properties are associated with a single piece of data
+        // can be implmented by fully implementing or auto implementing them
+
+        // fully implemented has additional logic to execute for the control of the data, usually validation
+        // fully implemented will also have declared data members to store data
+        // auto implemented will not have additional logic
+        // auto implemented will not have a declared data member to store the data, instead the OS will create based on the property only accessible by the property
+
+        public string Title
+        {
+            // accessor
+            get { return _Title; }
+            // mutator
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Title is required.");
+                }
+                _Title = value;
+            }
+        }
+    }
+}
