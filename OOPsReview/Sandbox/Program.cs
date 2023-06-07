8b16b6971c3dc9c6cd8106186f044181f79d3882
Cyboran <cyboran@live.ca>
2023-06-07 17:35:53 -0600

    added methods to sandbox program

--- OOPsReview/Sandbox/Program.cs
+++ OOPsReview/Sandbox/Program.cs
@@ -1,62 +1,82 @@
 ﻿using OOPsReview;
+// Check Don's video for what he did here, something to do with RecordSample() and RefactorSample()
 
-// example of using a record
-
-Residence home = new Residence(20, "Catalina Court", "Fort Sasketchewan", "AB", "T8L0E9");
-Console.WriteLine(home.ToString());
-
-// the values in home cannot be changed through normal means.
-// instead, you must create a new instance to change the values
-
-home = new Residence(25, "Catalina Court", "Fort Sasketchewan", "AB", "T8L0E9");
-Console.WriteLine(home.ToString());
-
-// example of refactoring which is a process of restructuring code while not changing the original functionality, the purpose of this
-bool testFlag;
-
-// original code
-
-if (home.prov.ToLower() == "AB")
+void RecordSample()
 {
-    testFlag = true;
-}
-else if (home.prov.ToLower() == "BC")
-{
-    testFlag = true;
-}
-else if (home.prov.ToLower() == "SK")
-{
-    testFlag = true;
-}
-else if (home.prov.ToLower() == "MN")
-{
-    testFlag = true;
-}
+    // example of using a record
 
-// improved code with if statement
-if (home.prov.ToLower() == "AB" ||
-    home.prov.ToLower() == "BC" ||
-    home.prov.ToLower() == "SK" ||
-    home.prov.ToLower() == "MN")
-{
-    testFlag = true;
-}
-else
-{
-    testFlag = false;
-}
+    Residence home = new Residence(20, "Catalina Court", "Fort Sasketchewan", "AB", "T8L0E9");
+    Console.WriteLine(home.ToString());
 
-// improved code with switch statement
+    // the values in home cannot be changed through normal means.
+    // instead, you must create a new instance to change the values
 
-switch (home.prov.ToLower())
-{
-    case "AB":
-    case "BC":
-    case "SK":
-    case "MN":
+    home = new Residence(25, "Catalina Court", "Fort Sasketchewan", "AB", "T8L0E9");
+    Console.WriteLine(home.ToString());
+
+    // example of refactoring which is a process of restructuring code while not changing the original functionality, the purpose of this
+    bool testFlag;
+
+    // original code
+
+    if (home.prov.ToLower() == "AB")
+    {
         testFlag = true;
-        break;
-    default:
+    }
+    else if (home.prov.ToLower() == "BC")
+    {
+        testFlag = true;
+    }
+    else if (home.prov.ToLower() == "SK")
+    {
+        testFlag = true;
+    }
+    else if (home.prov.ToLower() == "MN")
+    {
+        testFlag = true;
+    }
+}
+void RefactorSample()
+{
+    // example of using a record
+
+    Residence home = new Residence(20, "Catalina Court", "Fort Sasketchewan", "AB", "T8L0E9");
+    Console.WriteLine(home.ToString());
+
+    // the values in home cannot be changed through normal means.
+    // instead, you must create a new instance to change the values
+
+    home = new Residence(25, "Catalina Court", "Fort Sasketchewan", "AB", "T8L0E9");
+    Console.WriteLine(home.ToString());
+
+    // example of refactoring which is a process of restructuring code while not changing the original functionality, the purpose of this
+    bool testFlag;
+
+    // improved code with if statement
+    if (home.prov.ToLower() == "AB" ||
+        home.prov.ToLower() == "BC" ||
+        home.prov.ToLower() == "SK" ||
+        home.prov.ToLower() == "MN")
+    {
+        testFlag = true;
+    }
+    else
+    {
         testFlag = false;
-        break;
+    }
+
+    // improved code with switch statement
+
+    switch (home.prov.ToLower())
+    {
+        case "AB":
+        case "BC":
+        case "SK":
+        case "MN":
+            testFlag = true;
+            break;
+        default:
+            testFlag = false;
+            break;
+    }
 }
