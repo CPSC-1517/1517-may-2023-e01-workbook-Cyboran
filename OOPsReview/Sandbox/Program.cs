e8f32076a2fbca5a53c5437840af77fac2ebb413
Cyboran <cyboran@live.ca>
2023-06-07 18:17:46 -0600

    updated program with file I/O examples

--- OOPsReview/Sandbox/Program.cs
+++ OOPsReview/Sandbox/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OOPsReview;
 // Check Don's video for what he did here, something to do with RecordSample() and RefactorSample()
-
+//RecordSample();
+//RefactorSample();
+FileIOCSV();
 void RecordSample()
 {
     // example of using a record
@@ -79,4 +81,62 @@ void RefactorSample()
             testFlag = false;
             break;
     }
+}
+void FileIOCSV()
+{
+    // create list
+    List <Employment> emp = new List <Employment>();
+    emp.Add(new Employment("SAS Member", (SupervisoryLevel)Enum.Parse(typeof(SupervisoryLevel), "TeamMember"), DateTime.Parse("2015/06/14"), 3.6));
+    emp.Add(new Employment("SAS Lead", (SupervisoryLevel)Enum.Parse(typeof(SupervisoryLevel), "TeamLeader"), DateTime.Parse("2019/01/24"), 2.8));
+    emp.Add(new Employment("SAS Lead", (SupervisoryLevel)Enum.Parse(typeof(SupervisoryLevel), "Supervisor"), DateTime.Parse("2021/09/24"), 1.8));
+
+    DumpEmployments(emp);
+
+    WriteEmploymentCollectionToCSV(emp);
+
+    List<Employment> empRead = new List <Employment>();
+    empRead = ReadEmploymentCollectionFromCSV();
+}
+void DumpEmployments(List<Employment> emp)
+{
+    Console.WriteLine("\n\t\tDump of Employment Instances:\n");
+    for (int i = 0; i < emp.Count; i++)
+    {
+        Console.WriteLine($"Instance {i}:\t {emp[i].ToString()}");
+    }
+}
+void WriteEmploymentCollectionToCSV(List<Employment> emp)
+{
+    // using StreamReader and StreamWriter in conjuction with .AppendAllLines
+    // filepath: C:\Temp\EmploymentData.txt
+    string filePath = @"C:\Temp\EmploymentData.txt";
+
+    // converting List into string List
+    List<string> empCollection = new List<string>();
+    foreach(Employment emps in emp)
+    {
+        empCollection.Add(emps.ToString());
+    }
+
+    File.AppendAllLines(filePath, empCollection);
+}
+List<Employment> ReadEmploymentCollectionFromCSV()
+{
+    // using StreamReader and StreamWriter in conjuction with .AppendAllLines
+    // filepath: C:\Temp\EmploymentData.txt
+    string filePath = @"C:\Temp\EmploymentData.txt";
+    Employment empInstance = null;
+
+    string[] empCSVString = File.ReadAllLines(filePath);
+
+    // convert array into employment instances
+
+    List<Employment> empCollection = new List<Employment>();
+    foreach (string line in empCSVString)
+    {
+        empInstance = Employment.Parse(line);
+        empCollection.Add(empInstance);
+    }
+
+    return empCollection;
 }
